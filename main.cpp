fc58d6d3a0a4a79928760034a7403251e0765563
Alex <alexDamian99@bitbucket.com>
2018-11-28 19:09:13 +0200

    empty project

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <winbgim.h>
+using namespace std;
+
+int tabla[7][7] = {
+{1,0,0,1,0,0,1},
+{0,1,0,1,0,1,0},
+{0,0,1,1,1,0,0},
+{1,1,1,0,1,1,1},
+{0,0,1,1,1,0,0},
+{0,1,0,1,0,1,0},
+{1,0,0,1,0,0,1},
+};
+
+int main (){
+   bool playable = true;
+   //while(playable){
+
+   //}
+
+   unsigned int l = 0;
+   initwindow(800, 600);
+   cin>>l;
+
+   return 0;
+}
